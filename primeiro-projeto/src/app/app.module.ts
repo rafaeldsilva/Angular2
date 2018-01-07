952ab75fb97405a52ea598f940bd07784bf85de2
Rafael Silva <rafael.dsilva@outlook.com.br>
2018-01-07 21:49:37 -0200

    Criação das Views (Componentes)

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
+import { ServicoComponent } from './servico/servico.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SobreComponent,
+    ContatoComponent,
+    ServicoComponent
   ],
   imports: [
     BrowserModule
